93607c4c8338bf205d4f6204b601cd520c78f488
mahesh <mail.mahesh.lk@gmail.com>
2021-11-26 10:07:03 +0800

    solid-spr

--- spr/sprissue.go
+++ spr/sprissue.go
@@ -13,9 +13,13 @@ func (c circle) Area() {
 	fmt.Printf("Area of: %f\n ", math.Pi*c.radius*c.radius)
 }
 
+func NewCircle() circle {
+	c := &circle{}
+	return *c
+}
+
 func main() {
-	c := circle{
-		radius: 3.0,
-	}
+	c := NewCircle()
+	c.radius = 3.0
 	c.Area()
 }
