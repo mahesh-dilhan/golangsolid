d16844ecd30fc5a72f813b05e74e399e8852f4cc
mahesh <mail.mahesh.lk@gmail.com>
2021-12-16 12:02:32 +0800

    solid-di

--- di/main.go
+++ di/main.go
@@ -14,6 +14,6 @@ type IPhone struct {
 	s    *Storage
 }
 
-func NewIphone(name string, s *Storage) *Phone {
-
+func NewIphone(name string, s *Storage) *IPhone {
+	p := IPhone{name: "iPhone13", s: s}
 }
