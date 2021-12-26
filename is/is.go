edf6bc24f967a975d4ee3c3f833a7c02044c7b03
mahesh <mail.mahesh.lk@gmail.com>
2021-12-26 13:45:37 +0800

    solid-is

--- is/is.go
+++ is/is.go
@@ -1,5 +1,7 @@
 package is
 
+import "fmt"
+
 type Phone interface {
 	call()
 }
@@ -16,4 +18,12 @@ type Lineup interface {
 func (ph *IPhone) boot() {
 	ph.doPOST()
 	ph.checkIO()
+}
+
+func (ph *IPhone) doPOST() {
+	fmt.Println("[boot] do power on self test")
+}
+
+func (ph *IPhone) checkIO() {
+	fmt.Println("[boot] check Input and output")
 }
